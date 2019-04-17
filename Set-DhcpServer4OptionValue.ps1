f6b2724edf22cf5ca862e24e60629077342979e4
Mark Holzapfel <mholzapfel@npauctions.com>
2019-04-17 11:14:36 -0700

    test5

--- Set-DhcpServer4OptionValue.ps1
+++ Set-DhcpServer4OptionValue.ps1
@@ -4,22 +4,22 @@
 # Set-DhcpServerv4OptionValue -ScopeId 10.10.10.0 -OptionId 43 -Value "241","08","33","255","132","10","33","255","133"
 # https://docs.microsoft.com/en-us/powershell/module/dhcpserver/set-dhcpserverv4optionvalue?view=win10-ps
 # example2
-# Set-DhcpServerv4OptionValue -ScopeId 10.249.232.0 -OptionId 66 -Value "https://pp.ringcentral.com/pp"
+Set-DhcpServerv4OptionValue -ScopeId 10.249.232.0 -OptionId 66 -Value "https://pp.ringcentral.com/pp"
 # Set-DhcpServerv4OptionValue
-   [-PolicyName <String>]
-   [-PassThru]
-   [-Force]
-   [-ReservedIP <IPAddress>]
-   [[-ScopeId] <IPAddress>]
-   [-UserClass <String>]
-   [-ComputerName <String>]
-   [-VendorClass <String>]
-   [-Value] <String[]>
-   [-OptionId] <UInt32>
-   [-CimSession <CimSession[]>]
-   [-ThrottleLimit <Int32>]
-   [-AsJob]
-   [-WhatIf]
-   [-Confirm]
-   [<CommonParameters>]
+ #  [-PolicyName <String>]
+ #  [-PassThru]
+ #  [-Force]
+ #  [-ReservedIP <IPAddress>]
+ #  [[-ScopeId] <IPAddress>]
+ #  [-UserClass <String>]
+ #  [-ComputerName <String>]
+ #  [-VendorClass <String>]
+ #  [-Value] <String[]>
+ #  [-OptionId] <UInt32>
+ #  [-CimSession <CimSession[]>]
+ #  [-ThrottleLimit <Int32>]
+ #  [-AsJob]
+ #  [-WhatIf]
+ #  [-Confirm]
+ #  [<CommonParameters>]
    

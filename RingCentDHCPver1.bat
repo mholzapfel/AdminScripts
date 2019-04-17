be9471d6020d798600821d4d4e678026ee09514e
Mark Holzapfel <mholzapfel@npauctions.com>
2019-04-17 11:25:11 -0700

    q

--- RingCentDHCPver1.bat
+++ RingCentDHCPver1.bat
@@ -4,7 +4,9 @@
 # Set-DhcpServerv4OptionValue -ScopeId 10.10.10.0 -OptionId 43 -Value "241","08","33","255","132","10","33","255","133"
 # https://docs.microsoft.com/en-us/powershell/module/dhcpserver/set-dhcpserverv4optionvalue?view=win10-ps
 # example2
-Set-DhcpServerv4OptionValue -ScopeId 10.249.232.0 -OptionId 66 -Value "https://pp.ringcentral.com/pp"
+@ECHO OFF
+PowerShell.exe -Command "Set-DhcpServerv4OptionValue -ScopeId 10.249.232.0 -OptionId 66 -Value "https://pp.ringcentral.com/pp""
+PAUSE
 # Set-DhcpServerv4OptionValue
 #  [-PolicyName <String>]
 #  [-PassThru]
